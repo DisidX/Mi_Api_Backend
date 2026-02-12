4bb3b82838d47b7874e40a2238f62a1e8d8ec66f
DisidX <mipersonal339@gmail.com>
2026-02-12 12:30:36 -0500

    Fix: correccion de var DATA_FILE and structure initial of productos.json..

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const express = require("express");
 const app = express();
-const fs = require("fs"); // File System Reader 
+const fs = require("fs"); // File System Reader
 const PORT = 3000;
-
 
 app.use(express.json());
 
-const DATA_FILE = './productos.json';
+const DATA_FILE = "./productos.json";
 
-app.get('/productos',(req,res)=>{
-    const data = fs.readFileSync('DATA_FILE','utf-8');
-    const productos =  JSON.parse(data);
-    res.json(productos)
-})
+app.get("/productos", (req, res) => {
+    const data = fs.readFileSync(DATA_FILE, "utf-8");
+    const productos = JSON.parse(data);
+    res.json(productos);
+});
 
-app.post('/productos', (req, res)=>{
+app.get("/", (req, res) => {
+    res.send("Page Home - API");
+});
+
+app.post("/productos", (req, res) => {
     const nuevoProducto = req.body;
 
-    const data = fs.readFileSync('DATA_FILE','utf-8');
+    const data = fs.readFileSync(DATA_FILE, "utf-8");
     const productos = JSON.parse(data);
-    
+
     productos.push(nuevoProducto);
-    fs.writeFileSync(DATA_FILE,JSON.stringify(productos,null,2));
-    res.status(201).json({mensaje: "Producto creado!",producto: nuevoProducto});
+    fs.writeFileSync(DATA_FILE, JSON.stringify(productos, null, 2));
+    res
+        .status(201)
+        .json({ mensaje: "Producto creado!", producto: nuevoProducto });
 });
 
-
-
-
-app.listen(PORT,()=>{
-    console.log(`Servidor que corre en http://localhost:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Servidor que corre en http://localhost:${PORT}`);
 });
-
-
